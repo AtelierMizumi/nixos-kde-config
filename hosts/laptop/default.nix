14cf763cc705d2c46de33cffee3c6d2033014afe
AtelierMizumi <thuanc177@gmail.com>
2025-05-06 14:45:18 +0700

    init

---
+++ hosts/laptop/default.nix
@@ -0,0 +1,35 @@
+{ pkgs, config, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./../../modules/core
+  ];
+
+  environment.systemPackages = with pkgs; [
+    acpi
+    powertop
+  ];
+
+  services = {
+    power-profiles-daemon.enable = true;
+    
+    upower = {
+      enable = true;
+      percentageLow = 30;
+      percentageCritical = 10;
+      percentageAction = 5;
+      criticalPowerAction = "PowerOff";
+    };
+  };
+
+  boot = {
+    kernelModules = [ "acpi_call" ];
+    extraModulePackages =
+      with config.boot.kernelPackages;
+      [
+        acpi_call
+        cpupower
+      ]
+      ++ [ pkgs.cpupower-gui ];
+  };
+}
