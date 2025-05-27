fe181d1db43728e19453d7b108fe45c27cbcf159
AtelierMizumi <thuanc177@gmail.com>
2025-05-27 15:19:02 +0700

    Re-enable systemd-boot settings in bootloader configuration

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 {
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
-  # boot.loader.systemd-boot.configurationLimit = 10;
-  # boot.supportedFilesystems = [ "ntfs" ];
-
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
+  boot.supportedFilesystems = [ "ntfs" ];
+  /*
   boot.loader = {
     grub = {
       enable = true;
@@ -25,4 +25,5 @@
       '';
     };
   };
+  */
 }
