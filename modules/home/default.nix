8a491c18612a9124507ae8f57732b6de073b4630
AtelierMizumi <thuanc177@gmail.com>
2025-05-31 17:56:27 +0700

    Update module imports in default.nix: add zram and reorganize home modules

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,19 +1,23 @@
 { ... }:
 {
   imports = [
-    # better cat command
     ./browser.nix # firefox based browser
     ./cava.nix # audio visualizer
     ./discord.nix # discord
+    ./docker.nix # docker configuration
     ./fastfetch.nix # fetch tool
+    ./fcitx5.nix # input method
     ./fonts.nix # fonts settings
     ./gaming.nix # packages related to gaming
     ./git.nix # version control
     ./kitty.nix # terminal
     ./nix-search/nix-search.nix # TUI to search nixpkgs
+    ./nvim.nix # neovim configuration
     ./packages # other packages
+    ./proton-vpn.nix # protonvpn gui
     ./ssh.nix
     ./vscode # vscode fork
     ./xdg-mimes.nix # xdg config
+    ./xserver.nix # x server configuration
   ];
 }
