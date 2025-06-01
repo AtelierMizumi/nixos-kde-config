d49b4ac14b51f1ab2c8177c05e9d683d8df0ce38
AtelierMizumi <thuanc177@gmail.com>
2025-06-01 17:56:52 +0700

    Refactor fcitx5 configuration: update session variables and clean up commented-out package list

--- modules/home/fcitx5.nix
+++ modules/home/fcitx5.nix
@@ -1,12 +1,20 @@
 { pkgs, ... }:
 {
-  environment.sessionVariables = {
+  home.sessionVariables = {
     # fcitx5
-    XMODIFIERS = "@im=fcitx5";
     QT_IM_MODULE = "fcitx5";
     GTK_IM_MODULE = "fcitx5";
     SDL_IM_MODULE = "fcitx";
   };
+
+  # environment.systemPackages = with pkgs; [
+  #   fcitx5
+  #   fcitx5-gtk
+  #   fcitx5-qt
+  #   fcitx5-unikey
+  #   fcitx5-anthy
+  #   fcitx5-configtool
+  # ];
 
   i18n.inputMethod = {
     type = "fcitx5";
@@ -14,6 +22,7 @@
     fcitx5 = {
       waylandFrontend = true;
       addons = with pkgs; [
+        fcitx5-skk-qt
         fcitx5-unikey
         fcitx5-anthy
         fcitx5-configtool
