28a1a4b57cd4eef10633680b12803bfc9cc7f424
AtelierMizumi <thuanc177@gmail.com>
2025-06-04 17:50:58 +0700

    Refactor zram configuration: remove writebackDevice setting

--- modules/core/zram.nix
+++ modules/core/zram.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ ... }:
 {
   boot.kernel.sysctl = {
     "vm.swappiness" = 10;
@@ -7,6 +7,5 @@
   zramSwap = {
     enable = true;
     memoryMax = 16 * 1024 * 1024 * 1024;
-    writebackDevice = "/dev/nvme0n1p3";
   };
 }
