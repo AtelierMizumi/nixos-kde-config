602770d9e9229c16085f28449f86e574a2e33301
AtelierMizumi <thuanc177@gmail.com>
2025-07-05 06:25:00 +0700

    Add OBS Studio plugin configuration for enhanced functionality

--- modules/home/packages/gui.nix
+++ modules/home/packages/gui.nix
@@ -16,4 +16,13 @@
     # translation
     dialect
   ];
+
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+    ];
+  };
 }
