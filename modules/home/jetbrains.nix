3e8163fc4744b5cda33705bbfbe5786e77e8f8af
AtelierMizumi <thuanc177@gmail.com>
2025-08-26 22:22:43 +0700

    Update JetBrains configuration: add additional packages for development tools and libraries

--- modules/home/jetbrains.nix
+++ modules/home/jetbrains.nix
@@ -4,5 +4,14 @@
     jetbrains.idea-ultimate
     jetbrains.rider
     github-copilot-intellij-agent
+    dotnet-sdk_9
+    libcutl
+    mono
+    wasm-tools
+    android-tools
+    # Required for SkiaSharp and Avalonia on Linux
+    fontconfig
+    freetype
+    icu.dev
   ];
 }
